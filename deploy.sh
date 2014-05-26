7bb86caf4b37ba988eb63faa1e69da551edb6028
Noah Freitas <noah.j.freitas@gmail.com>
2014-05-26 15:57:31 -0700

    Add Marlowe page.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 cp app/*.html dist/
 cp app/views/*.html dist/views/
+cp app/images/*.jpg dist/images/
 cp app/*.json dist/
 lsc -co dist/js app/scripts/*.ls
 lsc -co dist/js/controllers app/scripts/controllers/*.ls
